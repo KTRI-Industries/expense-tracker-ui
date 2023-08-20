53a65eaaa10600c32ee4a5de4e0bd3fc7624d242
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-08-20 18:29:33 +0300

    feat #8: Generate standalone ngrx feature store for auth library.

--- libs/shared/auth/src/index.ts
+++ libs/shared/auth/src/index.ts
@@ -1,3 +1,12 @@
+import * as AuthActions from './lib/+state/auth.actions';
+
+import * as AuthFeature from './lib/+state/auth.reducer';
+
+import * as AuthSelectors from './lib/+state/auth.selectors';
+
+export * from './lib/+state/auth.models';
+
+export { AuthActions, AuthFeature, AuthSelectors };
 export * from './lib/lib.routes';
 
 export * from './lib/auth/auth.component';
--- libs/shared/auth/src/lib/lib.routes.ts
+++ libs/shared/auth/src/lib/lib.routes.ts
@@ -1,4 +1,17 @@
 import { Route } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import * as fromAuth from './+state/auth.reducer';
+import { AuthEffects } from './+state/auth.effects';
 
-export const authRoutes: Route[] = [{ path: '', component: AuthComponent }];
+export const authRoutes: Route[] = [
+  {
+    path: '',
+    component: AuthComponent,
+    providers: [
+      provideState(fromAuth.AUTH_FEATURE_KEY, fromAuth.authReducer),
+      provideEffects(AuthEffects),
+    ],
+  },
+];
