4c1c3d7eeba1ac559eddb2d7f961efce2aabbe50
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-08-25 01:05:07 +0300

    feat #8: Switch to action group.

--- libs/shared/auth/src/index.ts
+++ libs/shared/auth/src/index.ts
@@ -1,4 +1,3 @@
-import * as AuthActions from './lib/+state/auth.actions';
 
 import * as AuthFeature from './lib/+state/auth.reducer';
 
@@ -6,8 +5,9 @@ import * as AuthSelectors from './lib/+state/auth.selectors';
 
 export * from './lib/+state/auth.models';
 
-export { AuthActions, AuthFeature, AuthSelectors };
+export { AuthFeature, AuthSelectors };
 export * from './lib/lib.routes';
+export * from './lib/+state/auth.actions';
 
 export * from './lib/auth/auth.component';
 export * from './lib/user-info/user-info.component';
