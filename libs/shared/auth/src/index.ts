a8bb524048218040b853f771aecc3577d8ead51f
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-08-27 17:16:41 +0300

    feat #8: Cleanup.

--- libs/shared/auth/src/index.ts
+++ libs/shared/auth/src/index.ts
@@ -2,13 +2,10 @@ import * as AuthFeature from './lib/+state/auth.reducer';
 
 import * as AuthSelectors from './lib/+state/auth.selectors';
 
-export * from './lib/+state/auth.models';
-
 export { AuthFeature, AuthSelectors };
-export * from './lib/lib.routes';
+
 export * from './lib/+state/auth.actions';
 
-export * from './lib/auth/auth.component';
 export * from './lib/user-info/user-info.component';
 
 export * from './lib/+state/auth.effects';
