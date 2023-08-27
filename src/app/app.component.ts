32f539dd177e20c420bf3655be91a3b025309223
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-08-27 16:57:43 +0300

    feat #8: Login through store and effects.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { KeycloakService } from 'keycloak-angular';
+import { from } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AuthActions } from '@expense-tracker-ui/shared/auth';
 
 @Component({
   standalone: true,
@@ -9,6 +13,16 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'expense-tracker-ui';
+  constructor(private keycloak: KeycloakService, private store: Store) {}
+
+  ngOnInit(): void {
+    // TODO is there a better way to do this from outside the component for example?
+    from(this.keycloak.isLoggedIn()).subscribe((isLoggedIn) => {
+      if (isLoggedIn) {
+        this.store.dispatch(AuthActions.loginSuccess());
+      }
+    });
+  }
 }
