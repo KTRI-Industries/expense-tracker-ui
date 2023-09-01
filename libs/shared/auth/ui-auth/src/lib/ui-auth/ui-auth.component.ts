3e82883094a0066345f1ec054816841c0b639e22
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-09-01 21:44:33 +0300

    feat #8: Generate ui-auth library for presentational components.

---
+++ libs/shared/auth/ui-auth/src/lib/ui-auth/ui-auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'expense-tracker-ui-ui-auth',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ui-auth.component.html',
+  styleUrls: ['./ui-auth.component.css'],
+})
+export class UiAuthComponent {}
