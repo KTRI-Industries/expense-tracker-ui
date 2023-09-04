f609dd19e81cf03deef244059933cda1544d5eb6
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-09-04 20:19:57 +0300

    feat #8: Lazy load playground lib.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,19 +18,14 @@ import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
-import {
-  KeycloakPlaygroundEffects,
-  PlayGroundFeature,
-} from '@expense-tracker-ui/keycloak-playground';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
-    provideEffects(AuthEffects, KeycloakPlaygroundEffects),
+    provideEffects(AuthEffects),
     provideStore({ router: routerReducer }),
     provideRouterStore(),
     provideState(AuthFeature.authFeature),
-    provideState(PlayGroundFeature.playgroundFeature),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     KeycloakService,
     {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('@expense-tracker-ui/keycloak-playground').then(
+        (m) => m.keycloakPlaygroundRoutes
+      ),
+    // canActivate: [AppGuardGuard],
+    // data: {
+    //   roles: ['administrator'],
+    // },
+  },
+];
