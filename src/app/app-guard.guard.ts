351f02a78ef24aeb201f18256e67463e74b39575
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-09-04 20:21:04 +0300

    feat #8: Enable initial Auth Guard.

---
+++ src/app/app-guard.guard.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { KeycloakAuthGuard, KeycloakService } from 'keycloak-angular';
+import { Store } from "@ngrx/store";
+import { AuthActions } from "@expense-tracker-ui/shared/auth/data-access";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppGuardGuard extends KeycloakAuthGuard {
+  constructor(
+    protected override readonly router: Router,
+    protected readonly keycloak: KeycloakService,
+    private store: Store
+  ) {
+    super(router, keycloak);
+  }
+
+  public async isAccessAllowed(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean | UrlTree> {
+    // Force the user to log in if currently unauthenticated.
+    if (!this.authenticated) {
+      this.store.dispatch(AuthActions.login())
+    }
+
+    // Get the roles required from the route.
+    const requiredRoles = route.data['roles'];
+
+    // Allow the user to proceed if no additional roles are required to access the route.
+    if (!Array.isArray(requiredRoles) || requiredRoles.length === 0) {
+      return true;
+    }
+
+    // Allow the user to proceed if all the required roles are present.
+    return requiredRoles.every((role) => this.roles.includes(role));
+  }
+}
