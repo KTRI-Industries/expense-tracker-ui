940ab7a015a4f8aa8fddc7aaed56a12a179c55e1
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-09-04 20:19:57 +0300

    feat #8: Lazy load playground lib.

--- libs/keycloak-playground/src/lib/lib.routes.ts
+++ libs/keycloak-playground/src/lib/lib.routes.ts
@@ -1,6 +1,17 @@
 import { Route } from '@angular/router';
 import { KeycloakPlaygroundComponent } from './keycloak-playground/keycloak-playground.component';
+import { provideEffects } from '@ngrx/effects';
+import { KeycloakPlaygroundEffects } from './+state/keycloak-playground.effects';
+import { provideState } from '@ngrx/store';
+import { playgroundFeature } from './+state/keycloak-playground.reducer';
 
 export const keycloakPlaygroundRoutes: Route[] = [
-  { path: '', component: KeycloakPlaygroundComponent },
+  {
+    path: '',
+    component: KeycloakPlaygroundComponent,
+    providers: [
+      provideState(playgroundFeature),
+      provideEffects(KeycloakPlaygroundEffects),
+    ],
+  },
 ];
