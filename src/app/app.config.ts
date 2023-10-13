8cfa3f968a3b3a1fff62553fc90fa6762db2c71e
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-10-13 19:02:34 +0200

    feat #12: Externalize configuration to be able to change it at runtime.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -57,3 +57,9 @@ function initializeKeycloak(keycloak: KeycloakService) {
       enableBearerInterceptor: true,
     });
 }
+
+/*This needs to be a class and not an interface to be used as provider (why though?)*/
+export class ExternalConfiguration {
+  allowedOrigins: string[] = [];
+  basePath = '';
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { appConfig, ExternalConfiguration } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+fetch('/assets/app.config.json')
+  .then((response) => response.json())
+  .then((externalConfig) => {
+    bootstrapApplication(AppComponent, {
+      ...appConfig,
+      providers: [
+        ...appConfig.providers,
+        { provide: ExternalConfiguration, useValue: externalConfig },
+      ],
+    }).catch((err) => console.error(err));
+  });
