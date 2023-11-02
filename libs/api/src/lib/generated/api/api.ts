69b913859e971e12103c64fef7156937ad54b1a9
Alexandros Trifyllis <a.trifyllis@gmail.com>
2023-11-02 12:16:50 +0100

    feat #10: Move all generated code to dedicated folder and exclude it from linting.

--- libs/api/src/lib/generated/api/api.ts
+++ libs/api/src/lib/generated/api/api.ts
@@ -1,4 +1,3 @@
 export * from './keycloakIntegrationController.service';
 import { KeycloakIntegrationControllerService } from './keycloakIntegrationController.service';
-
 export const APIS = [KeycloakIntegrationControllerService];
