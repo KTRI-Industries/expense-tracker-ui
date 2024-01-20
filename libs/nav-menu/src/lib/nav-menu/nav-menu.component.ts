377ceaa3c413ca2af346b624ee3ffaa48816d9dd
atrifyllis <atrifyllis@gmail.com>
2024-01-20 20:18:17 +0100

    feat #69: Generate nav-menu library vtv .

---
+++ libs/nav-menu/src/lib/nav-menu/nav-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'expense-tracker-ui-nav-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-menu.component.html',
+  styleUrl: './nav-menu.component.css',
+})
+export class NavMenuComponent {}
