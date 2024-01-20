691a400ed6ecbbc09516d1a96294db3a3613bc4f
atrifyllis <atrifyllis@gmail.com>
2024-01-20 21:33:03 +0100

    feat #69: Add initial mat-toolbar for navigation menu as a sticky header.

--- libs/nav-menu/src/lib/nav-menu/nav-menu.component.ts
+++ libs/nav-menu/src/lib/nav-menu/nav-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'expense-tracker-ui-nav-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbar],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css',
 })
