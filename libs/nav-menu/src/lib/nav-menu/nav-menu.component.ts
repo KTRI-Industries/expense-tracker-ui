f2a85c37cd983073f60861cf0cb6e2f7614232c9
atrifyllis <atrifyllis@gmail.com>
2024-01-20 21:58:47 +0100

    feat #69: Add login button and tailwind classes to render it on the right side of the toolbar.

--- libs/nav-menu/src/lib/nav-menu/nav-menu.component.ts
+++ libs/nav-menu/src/lib/nav-menu/nav-menu.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'expense-tracker-ui-nav-menu',
   standalone: true,
-  imports: [CommonModule, MatToolbar],
+  imports: [CommonModule, MatToolbar, MatIcon, MatIconButton],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css',
 })
