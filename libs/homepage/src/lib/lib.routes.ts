9fdf347d982ec8e0a5f17db0d95ed1b1d34c0f90
atrifyllis <atrifyllis@gmail.com>
2024-01-20 20:09:12 +0100

    feat #69: Generate homepage library.

---
+++ libs/homepage/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+
+export const homepageRoutes: Route[] = [
+  { path: '', component: HomepageComponent },
+];
