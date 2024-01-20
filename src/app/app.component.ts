3cff5244d3d072e66579e61800abaeb6db3b18d5
atrifyllis <atrifyllis@gmail.com>
2024-01-20 20:27:12 +0100

    feat #69: Render nav-bar component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,21 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { KeycloakService } from 'keycloak-angular';
 import { Store } from '@ngrx/store';
 import { AuthActions } from '@expense-tracker-ui/shared/auth';
+import {NavMenuComponent} from "@expense-tracker-ui/nav-menu";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NavMenuComponent],
   selector: 'expense-tracker-ui-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'expense-tracker-ui';
-  constructor(private keycloak: KeycloakService, private store: Store) {}
+  constructor(
+    private keycloak: KeycloakService,
+    private store: Store,
+  ) {}
 
   ngOnInit(): void {
     this.store.dispatch(AuthActions.checkLogin());
