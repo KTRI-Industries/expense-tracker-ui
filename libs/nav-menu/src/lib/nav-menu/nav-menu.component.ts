b327fa7047a8706d9ac9cc1ded7c6da45c3adcb3
atrifyllis <atrifyllis@gmail.com>
2024-01-21 13:31:39 +0100

    feat #69: Make login and logout buttons functional, fix height of toolbar and add put router-outlet inside a tailwind container.

--- libs/nav-menu/src/lib/nav-menu/nav-menu.component.ts
+++ libs/nav-menu/src/lib/nav-menu/nav-menu.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'expense-tracker-ui-nav-menu',
   standalone: true,
-  imports: [CommonModule, MatToolbar, MatIcon, MatIconButton],
+  imports: [CommonModule, MatToolbar, MatIcon, MatIconButton, MatTooltip],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css',
 })
 export class NavMenuComponent {
   @Input() username: string | null | undefined = '';
   @Input() isAuthenticated: boolean | null | undefined = false;
+
+  @Output() login = new EventEmitter<void>();
+  @Output() logout = new EventEmitter<void>();
+
+  onLogin() {
+    this.login.emit();
+  }
+
+  onLogout() {
+    this.logout.emit();
+  }
 }
