d718c87007f880116aa59ecec724a010f2efb2c4
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-01-22 17:03:23 +0100

    feat #69: Create an initial landing page.

--- libs/homepage/src/lib/homepage/homepage.component.ts
+++ libs/homepage/src/lib/homepage/homepage.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { AuthActions } from '@expense-tracker-ui/shared/auth';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'expense-tracker-ui-homepage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButton, RouterLink],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  constructor(private store: Store) {}
+  login() {
+    this.store.dispatch(AuthActions.login());
+  }
+}
