dd0ea8d1059e54d2fc0222ce178eb164a9d5f787
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-01-23 19:02:01 +0100

    feat #69: Use tailwind starting kit landing page.

--- libs/homepage/src/lib/homepage/landing-page/landing-page.component.ts
+++ libs/homepage/src/lib/homepage/landing-page/landing-page.component.ts
@@ -2,11 +2,20 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatCard } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'expense-tracker-ui-landing-page',
   standalone: true,
-  imports: [CommonModule, MatButton, RouterLink, NgOptimizedImage],
+  imports: [
+    CommonModule,
+    MatButton,
+    RouterLink,
+    NgOptimizedImage,
+    MatCard,
+    MatIcon,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
