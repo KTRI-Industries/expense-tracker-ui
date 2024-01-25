b660b2b7f66e461300ae5d96a5c72cf0c4ac0e7c
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-01-25 21:02:48 +0100

    feat #69: Add unit tests for nav-menu component.

--- libs/nav-menu/src/lib/nav-menu/nav-menu.component.spec.ts
+++ libs/nav-menu/src/lib/nav-menu/nav-menu.component.spec.ts
@@ -1,21 +1,70 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavMenuComponent } from './nav-menu.component';
+import { render, RenderResult, screen } from '@testing-library/angular';
+import { fireEvent } from '@testing-library/dom';
+import '@testing-library/jest-dom';
 
 describe('NavMenuComponent', () => {
-  let component: NavMenuComponent;
-  let fixture: ComponentFixture<NavMenuComponent>;
+  let component: RenderResult<NavMenuComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NavMenuComponent],
-    }).compileComponents();
+  it('should create NavMenuComponent', async () => {
+    component = await render(NavMenuComponent, {
+      imports: [], // Import necessary modules here
+      providers: [], // Provide any necessary dependencies here
+    });
 
-    fixture = TestBed.createComponent(NavMenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    expect(component.fixture.componentInstance).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display username if authenticated', async () => {
+    component = await setupComponent(true, 'JohnDoe');
+
+    expect(screen.getAllByText('JohnDoe')).toHaveLength(2);
+  });
+
+  it('should display "Login" button if not authenticated', async () => {
+    component = await setupComponent(false, null);
+
+    expect(screen.getByText('Login')).toBeInTheDocument();
+  });
+
+  it('should emit login event when "Login" button is clicked', async () => {
+    component = await setupComponent(false, null);
+    const loginSpy = jest.spyOn(component.fixture.componentInstance, 'onLogin');
+
+    fireEvent.click(screen.getAllByText('login')[0]); // two buttons with same text because of sidenav
+
+    expect(loginSpy).toHaveBeenCalled();
+  });
+
+  it('should display "Logout" button if authenticated', async () => {
+    component = await setupComponent(true, null);
+
+    expect(screen.getByText('Logout')).toBeInTheDocument();
+  });
+
+  it('should emit logout event when "Logout" button is clicked', async () => {
+    component = await setupComponent(true, null);
+    const logoutSpy = jest.spyOn(
+      component.fixture.componentInstance,
+      'onLogout',
+    );
+
+    fireEvent.click(screen.getAllByText('logout')[0]);
+
+    expect(logoutSpy).toHaveBeenCalled();
   });
 });
+
+async function setupComponent(
+  isAuthenticated: boolean,
+  username: string | null,
+) {
+  return await render(NavMenuComponent, {
+    imports: [], // Import necessary modules here
+    providers: [], // Provide any necessary dependencies here
+    componentProperties: {
+      isAuthenticated,
+      username,
+    },
+  });
+}
