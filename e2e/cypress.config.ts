3b62dac699d09b7fd2983b4adf52cfca0a2d981f
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-02-09 21:45:15 +0100

    feat #11: Configure initial cypress e2e test.

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
+const cypressJsonConfig = {
+  // fileServerFolder: '.',
+  // fixturesFolder: './src/fixtures',
+  // video: true,
+  // videosFolder: '../../dist/cypress/apps/senik-ui-e2e/videos',
+  // screenshotsFolder: '../../dist/cypress/apps/senik-ui-e2e/screenshots',
+  // chromeWebSecurity: false,
+  // defaultCommandTimeout: 20000,
+  // specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  // supportFile: 'src/support/e2e.ts',
+  experimentalRunAllSpecs: true,
+};
+
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname),
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    ...cypressJsonConfig,
+  },
 });
--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -8,6 +8,6 @@ describe('expense-tracker-ui', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome expense-tracker-ui');
+    getGreeting().contains('Expense Tracker');
   });
 });
