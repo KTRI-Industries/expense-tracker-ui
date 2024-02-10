1ee6d54a9a9f7644a26f716ff2234bade1d08ff7
atrifyllis <atrifyllis@gmail.com>
2024-02-10 13:33:07 +0100

    feat #77: Create transactions ngrx feature state.

--- libs/transactions/src/index.ts
+++ libs/transactions/src/index.ts
@@ -1,3 +1,12 @@
+import * as TransactionsActions from './lib/+state/transactions.actions';
+
+import * as TransactionsFeature from './lib/+state/transactions.reducer';
+
+import * as TransactionsSelectors from './lib/+state/transactions.selectors';
+
+export * from './lib/+state/transactions.models';
+
+export { TransactionsActions, TransactionsFeature, TransactionsSelectors };
 export * from './lib/lib.routes';
 
 export * from './lib/transactions/transactions.component';
--- libs/transactions/src/lib/lib.routes.ts
+++ libs/transactions/src/lib/lib.routes.ts
@@ -1,6 +1,20 @@
 import { Route } from '@angular/router';
 import { TransactionsComponent } from './transactions/transactions.component';
+import { provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import * as fromTransactions from './+state/transactions.reducer';
+import { TransactionsEffects } from './+state/transactions.effects';
 
 export const transactionsRoutes: Route[] = [
-  { path: '', component: TransactionsComponent },
+  {
+    path: '',
+    component: TransactionsComponent,
+    providers: [
+      provideState(
+        fromTransactions.TRANSACTIONS_FEATURE_KEY,
+        fromTransactions.transactionsReducer,
+      ),
+      provideEffects(TransactionsEffects),
+    ],
+  },
 ];
