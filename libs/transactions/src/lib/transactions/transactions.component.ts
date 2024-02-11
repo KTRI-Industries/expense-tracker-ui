2a8865c96eda9188a01c88508457b9d5d6556abc
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-02-11 12:40:55 +0100

    feat #77: Add sample transactions page.

--- libs/transactions/src/lib/transactions/transactions.component.ts
+++ libs/transactions/src/lib/transactions/transactions.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle,
+} from '@angular/material/card';
 
 @Component({
   selector: 'expense-tracker-ui-transactions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCard,
+    MatCardContent,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardHeader,
+  ],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css',
 })
