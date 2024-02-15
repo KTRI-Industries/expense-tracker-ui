72d13dd7e79f6e0f9b3558f68fd42a3db7df9651
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-02-15 13:59:52 +0100

    feat #77: Generate openapi for transactions endpoints.

--- libs/keycloak-playground/src/lib/keycloak-playground.service.ts
+++ libs/keycloak-playground/src/lib/keycloak-playground.service.ts
@@ -1,12 +1,16 @@
 import { inject, Injectable } from '@angular/core';
-import { APIS, UserInfo } from '@expense-tracker-ui/api';
+import {
+  APIS,
+  KeycloakIntegrationControllerService,
+  UserInfo,
+} from '@expense-tracker-ui/api';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class KeycloakPlaygroundService {
-  private api = inject(APIS[0]); // TODO better way to reference the generated code?
+  private api: KeycloakIntegrationControllerService = inject(KeycloakIntegrationControllerService); // TODO better way to reference the generated code?
 
   getAdmin(): Observable<UserInfo> {
     return this.api.admin();
