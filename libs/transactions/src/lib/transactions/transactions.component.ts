6363604d403eeeffd0e1fa1d7247d23905bc5ecd
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-02-16 12:00:41 +0100

    feat #80: Render transactions in responsive angular material table.

--- libs/transactions/src/lib/transactions/transactions.component.ts
+++ libs/transactions/src/lib/transactions/transactions.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   MatCard,
@@ -7,8 +7,21 @@ import {
   MatCardSubtitle,
   MatCardTitle,
 } from '@angular/material/card';
-import { PageTransactionDto } from '@expense-tracker-ui/api';
+import { PageTransactionDto, TransactionDto } from '@expense-tracker-ui/api';
 import { MatList, MatListItem } from '@angular/material/list';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+  MatTableDataSource,
+} from '@angular/material/table';
 
 @Component({
   selector: 'expense-tracker-ui-transactions',
@@ -22,10 +35,38 @@ import { MatList, MatListItem } from '@angular/material/list';
     MatCardHeader,
     MatList,
     MatListItem,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef,
   ],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css',
 })
-export class TransactionsComponent {
-  @Input() transactions: PageTransactionDto | null = null;
+export class TransactionsComponent implements OnInit {
+  @Input() transactions: PageTransactionDto | undefined;
+
+  protected transactionDatasource:
+    | MatTableDataSource<TransactionDto>
+    | undefined;
+
+  transactionColumns = [
+    'position',
+    'amount',
+    'description',
+    'date',
+    'category',
+  ];
+
+  ngOnInit(): void {
+    this.transactionDatasource = new MatTableDataSource(
+      this.transactions?.content,
+    );
+  }
 }
