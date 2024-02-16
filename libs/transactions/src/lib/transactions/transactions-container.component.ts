4586f5bcfaa9d6ff56558499e76d5eb7deb266e3
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-02-16 12:00:41 +0100

    feat #80: Render transactions in responsive angular material table.

--- libs/transactions/src/lib/transactions/transactions-container.component.ts
+++ libs/transactions/src/lib/transactions/transactions-container.component.ts
@@ -5,12 +5,14 @@ import { AsyncPipe } from '@angular/common';
 import { TransactionsComponent } from './transactions.component';
 
 @Component({
-  selector: 'transactions-container',
+  selector: 'expense-tracker-ui-transactions-container',
   standalone: true,
   imports: [TransactionsComponent, AsyncPipe],
   template: `
+    @if (transactions$ | async; as transactions) {
     <expense-tracker-ui-transactions
-      [transactions]="transactions$ | async"></expense-tracker-ui-transactions>
+      [transactions]="transactions"></expense-tracker-ui-transactions>
+    }
   `,
   styles: ``,
 })
