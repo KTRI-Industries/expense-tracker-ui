8b3bf736a6e08b159f172037f6ca469e0b6adcf0
atrifyllis <atrifyllis@gmail.com>
2024-03-21 20:07:25 +0100

    feat #83: WIP Load transaction from backend when not in store.

--- libs/transactions/src/lib/+state/transactions.reducer.ts
+++ libs/transactions/src/lib/+state/transactions.reducer.ts
@@ -1,5 +1,5 @@
 import { createFeature, createReducer, createSelector, on } from '@ngrx/store';
-import { PageTransactionDto } from '@expense-tracker-ui/api';
+import { PageTransactionDto, TransactionDto } from '@expense-tracker-ui/api';
 import { TransactionActions } from './transactions.actions';
 
 export const TRANSACTIONS_FEATURE_KEY = 'transactions';
@@ -25,6 +25,10 @@ export const transactionsFeature = createFeature({
         transactions,
       }),
     ),
+    on(TransactionActions.openTransactionFrom, (state) => ({
+      ...state,
+      selectedTransactionId: null, // otherwise the form will be prefilled with the last selected transaction
+    })),
     on(
       TransactionActions.createNewTransactionSuccess,
       (state, { transaction }) => ({
@@ -37,15 +41,30 @@ export const transactionsFeature = createFeature({
       ...state,
       selectedTransactionId: transactionId,
     })),
+    on(TransactionActions.loadTransaction, (state, { transactionId }) => ({
+      ...state,
+      selectedTransactionId: transactionId,
+    })),
+    on(TransactionActions.loadTransactionSuccess, (state, { transaction }) => ({
+      ...state,
+      transactions: {
+        ...state.transactions,
+        content: [...(state?.transactions?.content ?? []), transaction],
+      },
+    })),
   ),
   extraSelectors: ({ selectTransactions, selectSelectedTransactionId }) => ({
     selectCurrentTransaction: createSelector(
       selectTransactions,
       selectSelectedTransactionId,
-      (transactions, selectedTransactionId) =>
-        transactions?.content?.find(
+      (transactions, selectedTransactionId) => {
+        if (selectedTransactionId === null) {
+          return {} as TransactionDto; // this is only needed for the case of new transaction where we do not have a selected tx id but we still want to render the component. so we return an empty object instead of undefined...
+        }
+        return transactions?.content?.find(
           (t) => t.transactionId === selectedTransactionId,
-        ),
+        );
+      },
     ),
   }),
 });
--- libs/transactions/src/lib/transaction-container.component.ts
+++ libs/transactions/src/lib/transaction-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { TransactionActions } from './+state/transactions.actions';
 import { TransactionComponent } from './transaction.component';
@@ -9,23 +9,43 @@ import {
 import { selectCurrentTransaction } from './+state/transactions.selectors';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'expense-tracker-ui-transaction-container',
   standalone: true,
   imports: [TransactionComponent, AsyncPipe],
   template: `
+    @if (selectedTransaction$ | async; as selectedTransaction) {
     <expense-tracker-ui-transaction
-      [selectedTransaction]="selectedTransaction$ | async"
+      [selectedTransaction]="selectedTransaction"
       (create)="onCreate($event)"></expense-tracker-ui-transaction>
+    }
   `,
   styles: ``,
 })
-export class TransactionContainerComponent {
+export class TransactionContainerComponent implements OnInit {
   selectedTransaction$: Observable<TransactionDto | undefined> =
     this.store.select(selectCurrentTransaction);
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+  ) {}
+
+  /**
+   * Load the transaction if the id is present in the route. (case where user navigates directly to url).
+   */
+  ngOnInit() {
+    const transactionId = this.route.snapshot.paramMap.get('id');
+    if (transactionId) {
+      this.store.dispatch(
+        TransactionActions.loadTransaction({
+          transactionId: transactionId,
+        }),
+      );
+    }
+  }
 
   onCreate($event: CreateTransactionCommand) {
     this.store.dispatch(
