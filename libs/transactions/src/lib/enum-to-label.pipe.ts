871edfa30e82eff863d14ecc898912b31ebafc99
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-03-27 00:25:42 +0200

    feat #83: Add enumToLabel pipe.

---
+++ libs/transactions/src/lib/enum-to-label.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/**
+ * Given an enum value and a record mapping enum to string,
+ * returns the string mapped to specific enum value.
+ */
+@Pipe({
+  name: 'enumToLabel',
+  standalone: true,
+})
+export class EnumToLabelPipe implements PipeTransform {
+  transform(value: unknown, map: Record<any, string>): string {
+    const enumElement: string =
+      Object.keys(map).find((key) => key == value) || '';
+    return map[enumElement];
+  }
+}
