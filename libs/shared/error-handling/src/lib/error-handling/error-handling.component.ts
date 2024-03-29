5f7286679cb81cb86a6d5b56e1a4bf54e8a0aa77
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-03-29 15:04:48 +0200

    feat #89: Generate shared error-handling library.

---
+++ libs/shared/error-handling/src/lib/error-handling/error-handling.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'expense-tracker-ui-error-handling',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './error-handling.component.html',
+  styleUrl: './error-handling.component.css',
+})
+export class ErrorHandlingComponent {}
