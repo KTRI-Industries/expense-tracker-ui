2869d44dd3b2be47d269e83cc4e4ac3b72c5950a
atrifyllis <atrifyllis@gmail.com>
2024-11-22 22:07:49 +0100

    feat #122: Generate openapi for main tx data dashboard.

--- libs/shared/api/src/lib/generated/api/api.ts
+++ libs/shared/api/src/lib/generated/api/api.ts
@@ -1,15 +1,1 @@
-export * from './recurrentTransactionController.service';
-import { RecurrentTransactionControllerService } from './recurrentTransactionController.service';
-import { TenantControllerService } from './tenantController.service';
-import { TransactionControllerService } from './transactionController.service';
-import { UserControllerService } from './userController.service';
-
-export * from './tenantController.service';
-export * from './transactionController.service';
-export * from './userController.service';
-export const APIS = [
-  RecurrentTransactionControllerService,
-  TenantControllerService,
-  TransactionControllerService,
-  UserControllerService,
-];
+export * from './dashboardController.service';import { DashboardControllerService } from './dashboardController.service';import { RecurrentTransactionControllerService } from './recurrentTransactionController.service';import { TenantControllerService } from './tenantController.service';import { TransactionControllerService } from './transactionController.service';import { UserControllerService } from './userController.service';export * from './recurrentTransactionController.service';export * from './tenantController.service';export * from './transactionController.service';export * from './userController.service';export const APIS = [  DashboardControllerService,  RecurrentTransactionControllerService,  TenantControllerService,  TransactionControllerService,  UserControllerService,];
