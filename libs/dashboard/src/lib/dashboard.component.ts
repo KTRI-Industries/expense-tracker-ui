cd818043b2e4ab5459635c6c24299e72de00b5cc
atrifyllis <atrifyllis@gmail.com>
2024-11-23 15:06:16 +0100

    feat #122: Display main dashboard data in homepage.

--- libs/dashboard/src/lib/dashboard.component.ts
+++ libs/dashboard/src/lib/dashboard.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardDto } from '@expense-tracker-ui/shared/api';
+import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'expense-tracker-ui-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCard, MatCardTitle, MatCardContent],
   templateUrl: './dashboard.component.html',
   styles: ``,
 })
 export class DashboardComponent {
-  @Input() dashboard: DashboardDto | undefined;
+  @Input() dashboard: DashboardDto | undefined | null;
 }
