42402b13442c1546a4cae1fd1fd17c370900c480
atrifyllis <atrifyllis@gmail.com>
2024-11-23 14:44:56 +0100

    feat #122: Retrieve dashboard data in homepage.

--- libs/dashboard/src/lib/dashboard.component.ts
+++ libs/dashboard/src/lib/dashboard.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardDto } from '@expense-tracker-ui/shared/api';
 
 @Component({
-  selector: 'lib-dashboard',
+  selector: 'expense-tracker-ui-dashboard',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styles: ``,
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  @Input() dashboard: DashboardDto | undefined;
+}
--- libs/dashboard/src/lib/lib.routes.ts
+++ libs/dashboard/src/lib/lib.routes.ts
@@ -1,8 +1,9 @@
+/*
 import { Route } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { provideState } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
-import * as fromDashboard from './+state/dashboard.reducer';
+import { dashboardFeature } from './+state/dashboard.reducer';
 import { DashboardEffects } from './+state/dashboard.effects';
 
 export const dashboardRoutes: Route[] = [
@@ -10,11 +11,9 @@ export const dashboardRoutes: Route[] = [
     path: '',
     component: DashboardComponent,
     providers: [
-      provideState(
-        fromDashboard.DASHBOARD_FEATURE_KEY,
-        fromDashboard.dashboardReducer,
-      ),
+      provideState(dashboardFeature),
       provideEffects(DashboardEffects),
     ],
   },
 ];
+*/
