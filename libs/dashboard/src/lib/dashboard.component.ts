8d534c499d60a9eb054c22a5cb08c8034cf0fd05
atrifyllis <atrifyllis@gmail.com>
2024-11-24 22:18:03 +0100

    feat #122: WIP Add dashboard range picker.

--- libs/dashboard/src/lib/dashboard.component.ts
+++ libs/dashboard/src/lib/dashboard.component.ts
@@ -5,6 +5,12 @@ import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
 import { ChartData, ChartOptions } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { ChartFormattingService } from './chart-formatting.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormlyFieldConfig,
+  FormlyFormOptions,
+  FormlyModule,
+} from '@ngx-formly/core';
 
 @Component({
   selector: 'expense-tracker-ui-dashboard',
@@ -15,9 +21,11 @@ import { ChartFormattingService } from './chart-formatting.service';
     MatCardTitle,
     MatCardContent,
     BaseChartDirective,
+    ReactiveFormsModule,
+    FormlyModule,
   ],
   templateUrl: './dashboard.component.html',
-  styles: ``,
+  styles: [],
 })
 export class DashboardComponent {
   @Input() chartData:
@@ -27,7 +35,54 @@ export class DashboardComponent {
 
   @Input() dashboard: DashboardDto | undefined | null = null;
 
-  constructor(private chartFormattingService: ChartFormattingService) {}
+  form: FormGroup;
+  model: any = { dateRange: 'lastYear' }; // Set default value to 'lastYear'
+  options: FormlyFormOptions = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      fieldGroupClassName: 'flex flex-row space-x-4',
+      fieldGroup: [
+        {
+          key: 'dateRange',
+          type: 'select',
+          props: {
+            label: 'Select Date Range',
+            options: [
+              { value: 'lastWeek', label: 'Last Week' },
+              { value: 'lastMonth', label: 'Last Month' },
+              { value: 'lastYear', label: 'Last Year' },
+              { value: 'custom', label: 'Custom Date Range' },
+            ],
+          },
+        },
+        {
+          key: 'startDate',
+          type: 'datepicker',
+          props: {
+            label: 'Start Date',
+            placeholder: 'Pick start date',
+          },
+          hideExpression: (model) => model.dateRange !== 'custom',
+        },
+        {
+          key: 'endDate',
+          type: 'datepicker',
+          props: {
+            label: 'End Date',
+            placeholder: 'Pick end date',
+          },
+          hideExpression: (model) => model.dateRange !== 'custom',
+        },
+      ],
+    },
+  ];
+
+  constructor(
+    private chartFormattingService: ChartFormattingService,
+    private fb: FormBuilder,
+  ) {
+    this.form = this.fb.group({});
+  }
 
   public doughnutChartOptions: ChartOptions<'doughnut'> = {
     responsive: true,
