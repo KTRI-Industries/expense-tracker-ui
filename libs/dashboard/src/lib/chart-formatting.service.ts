a6f7864f2a4b8ccdb3a31e6fadaa0408b5c72d8a
Alexandros Trifyllis <a.trifyllis@gmail.com>
2024-12-02 17:01:20 +0100

    feat #122: Show custom legend with percentages.

--- libs/dashboard/src/lib/chart-formatting.service.ts
+++ libs/dashboard/src/lib/chart-formatting.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
-import { TooltipItem } from 'chart.js';
+import { ChartData, ChartType } from 'chart.js';
 
 @Injectable({
   providedIn: 'root',
@@ -8,15 +8,40 @@ import { TooltipItem } from 'chart.js';
 export class ChartFormattingService {
   constructor(private currencyPipe: CurrencyPipe) {}
 
-  formatTooltipLabel(
-    context: TooltipItem<'doughnut'>,
-    currency: string,
-  ): string {
-    const value = context.raw || 0;
-    const formattedValue = this.currencyPipe.transform(
-      value as number,
-      currency,
-    );
-    return `${formattedValue}`;
+  formatTooltipLabel(value: number, currency: string): string {
+    return `${this.currencyPipe.transform(value, currency)}`;
   }
+
+  customiseLegend<T extends ChartType>(
+    chartData: ChartData<T, number[], string | string[]>,
+  ): ChartLegendItem[] {
+    const dataset = chartData.datasets[0];
+    const totalValue: number = dataset.data.reduce(
+      (sum: number, value: number) => sum + value,
+      0,
+    );
+
+    // Ensure backgroundColor is treated as an array
+    const backgroundColors = Array.isArray(dataset.backgroundColor)
+      ? (dataset.backgroundColor as string[])
+      : [];
+
+    const legendItems: ChartLegendItem[] =
+      chartData.labels?.map((label, index) => ({
+        label: label as string,
+        value: dataset.data[index],
+        color: backgroundColors[index],
+        percentage: totalValue ? (dataset.data[index] / totalValue) * 100 : 0,
+      })) || [];
+
+    // Sort legend items in descending order based on percentage
+    return legendItems.sort((a, b) => b.percentage - a.percentage);
+  }
+}
+
+export interface ChartLegendItem {
+  color: string;
+  label: string;
+  value: number;
+  percentage: number;
 }
---
+++ libs/dashboard/src/lib/chart-legend.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardDto } from '@expense-tracker-ui/shared/api';
+
+@Component({
+  selector: 'expense-tracker-ui-chart-legend',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chart-legend.component.html',
+  styles: [],
+})
+export class ChartLegendComponent {
+  @Input() items: {
+    color: string;
+    label: string | string[];
+    value: number;
+    percentage: number;
+  }[] = [];
+
+  @Input() dashboard!: DashboardDto | undefined | null;
+}
