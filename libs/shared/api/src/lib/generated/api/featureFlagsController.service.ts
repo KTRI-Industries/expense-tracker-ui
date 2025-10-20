6a06dc7d21502ceef064c4aacedcc7f62fcaee43
atrifyllis <atrifyllis@gmail.com>
2025-10-20 23:04:21 +0200

    refactor #15: Change variable declaration from const to let for basePath.

--- libs/shared/api/src/lib/generated/api/featureFlagsController.service.ts
+++ libs/shared/api/src/lib/generated/api/featureFlagsController.service.ts
@@ -45,7 +45,7 @@ export class FeatureFlagsControllerService {
   public encoder: HttpParameterCodec;
 
   constructor() {
-    const basePath = inject(BASE_PATH, { optional: true });
+    let basePath = inject(BASE_PATH, { optional: true });
     const configuration = inject(Configuration, { optional: true });
 
     if (configuration) {
